e264f4d56d70e4f70de26e3cb5bae76ca2ebefd2
ml3907 <35697920+ml3907@users.noreply.github.com>
2018-01-22 16:25:56 -0500

    welcome

--- sketch.js
+++ sketch.js
@@ -3,10 +3,24 @@ function setup() {
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  text("Welcome to the LAST semester!", 280, 240);
+  textSize(32);
+  fill(255, 204, 0);
+
+  ellipse(280, 180, 40, 40)
+  fill(255)
+
+  ellipse(292, 230, 5, 5)
+  fill(255)
+
+  ellipse(308, 230, 5, 5)
+  fill(255)
+
+  arc(290, 200, 12, 12, 0, HALF_PI);
+  
+  if (mousePressed) {
+  	text(Hello)
+  	textSize(20)
+  	fill(255)
   }
-  ellipse(mouseX, mouseY, 80, 80);
 }
