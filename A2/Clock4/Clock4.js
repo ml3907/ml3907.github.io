4013bb3aaf96b1a5426e3f6fa6a47722bbb62285
ml3907 <35697920+ml3907@users.noreply.github.com>
2018-01-31 23:48:25 -0500

    Clock4-update

--- A2/Clock4/Clock4.js
+++ A2/Clock4/Clock4.js
@@ -14,9 +14,19 @@ function draw() {
 
   if (h>=12){h=h-12}
   var angle_h1 = map(h, 0, 12, 0, 2*PI)
-  var angle_h2 = map(h+1, 0, 12, 0, 2*PI
+  var angle_h2 = map(h+1, 0, 12, 0, 2*PI)
 
-  translate(width/2,height/2)
+  translate(width/2,height/2-50)
+ 
+  push();
+  noStroke()
+  fill(255)
+  ellipse(0,0,220,220)
+  fill(255, 190, 0)
+  ellipse(0,0,205,205)
+  fill(0)
+  ellipse(0,0,8,8)
+  pop();
 
   push();
   rotate(angle_s-(PI-2))
@@ -33,13 +43,26 @@ function draw() {
   push();
   rotate(angle_h1-(PI-2))
   strokeWeight(2)
-  line(0, 0, 50, 0)
+  line(0, 0, 60, 0)
   pop();
 
   push();
+  stroke(150);
   rotate(angle_h2-(PI-2))
   strokeWeight(2)
-  fill(25, 25, 80, 80);
-  line(0, 0, 50, 0)
+  line(0, 0, 60, 0)
   pop();
+
+  textSize(15);
+  text("Current Time in New York: ", -160, 190)
+  text("Current Time in Beijing:", -160, 220)
+
+  push();
+  text(h+12 + ": " + m + ": " + s, 70, 190)
+  if (h>=12) {
+    text(h +":" + m + ":" + s, 70, 220)
+  } else {
+    text(h+1 +": " + m + ": " + s, 70, 220)
+  }
+  pop()  
 }
