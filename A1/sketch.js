34825edf1c99975894c4f2d7197e99b9644d015e
ml3907 <35697920+ml3907@users.noreply.github.com>
2018-01-31 19:32:40 -0500

    A1

---
+++ A1/sketch.js
@@ -0,0 +1,48 @@
+let angle = 0;
+
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  background(30, 20, 55, 55)
+
+  fill(200);
+  noStroke();
+  textSize(30);
+  text("Welcome to ", 30, 240);
+
+  fill(255, 204, 0);
+  noStroke();
+  textSize(30);
+  text("DATA VISUALIZATION!", 220, 240);
+
+  //these are the eyes.
+  fill(255);
+  ellipse(65, 110, 18, 18);
+  fill(255);
+  ellipse(95, 110, 18, 18);
+  
+  //these are the eyeballs.
+  fill(15);
+  ellipse(68, 113, 10, 10);
+  fill(15);
+  ellipse(98, 113, 10, 10);
+  
+  //this is the smiling mouth.  
+  noFill();
+  stroke(255);
+  arc(80, 140, 40, 25, 0, PI);
+
+  //this is the current time.
+  var h = hour();
+  var m = minute();
+  var s = second();
+  textSize(15);
+  text("Current Time: " + h + ": " + m + ": " + s, 30, 380)
+
+
+  var ma = map(s, 0, 100, 0, width);
+  fill(255, 204, 0);
+  rect(30, 400, ma, 5);
+}
