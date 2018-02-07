def5ce1eca3f57ce2a535cf5201b5c443d71a2ac
ml3907 <35697920+ml3907@users.noreply.github.com>
2018-02-06 23:49:42 -0500

    Clock2 Square Final

--- A2/Clock2/Clock2.js
+++ A2/Clock2/Clock2.js
@@ -5,16 +5,42 @@ function setup() {
 function draw() {
   background(200);
 
-  //this is the clock box.
-  fill(255, 204, 0);
-  rect(50, 250, 120, 40);
-
-  //this is the current time.
   var h = hour();
   var m = minute();
   var s = second();
-  fill(255)
-  textSize(20);
-  text("Current Time: ", 50, 200)
-  text(h + ": " + m + ": " + s,60, 280)
+
+  translate(width/2,height/2)
+
+  var angle_s = map(s, 0, 60, 0, 2*PI)
+  var angle_m = map(m, 0, 60, 0, 2*PI)
+  if (h>=12){h=h-12}
+  var angle_h = map(h, 0, 12, 0, 2*PI)
+
+  //this is hour square.
+  push();
+  stroke(100);
+  strokeWeight(2.5);
+  noFill();
+  rotate(angle_h-(PI-2));
+  rectMode(CENTER);
+  rect(0, 0, 150, 150);
+  pop();
+  //this is minute square.
+  push();
+  stroke(255, 190, 0);
+  strokeWeight(2.5);
+  noFill();
+  rotate(angle_m-(PI-2));
+  rectMode(CENTER);
+  rect(0, 0, 150, 150);
+  pop();
+  //this is second square.
+  push();
+  stroke(255);
+  strokeWeight(2.5);
+  noFill();
+  rotate(angle_s-(PI-2));
+  rectMode(CENTER);
+  rect(0, 0, 150, 150);
+  pop();
 }
