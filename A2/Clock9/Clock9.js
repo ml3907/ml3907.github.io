9ae5af163074cd944ad153b2c3fa76fe1fafc52a
ml3907 <35697920+ml3907@users.noreply.github.com>
2018-02-07 00:24:36 -0500

    clock9 try2

--- A2/Clock9/Clock9.js
+++ A2/Clock9/Clock9.js
@@ -1,16 +1,11 @@
-var second_xs = [], second_ys = [];
-
 function setup() {
   createCanvas(640, 480);
 }
 
 function draw() {
   background(200);
-  fill(255)
 
-  var h = hour();
-  var m = minute();
-  var s = second();
+  var second_xs = [], second_ys = [];
   
   for (var i = 1, i < 60; i++) {
     drawBall(second_xs[i], second_ys[i], 10, 190)
