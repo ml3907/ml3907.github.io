2d12c89e0a56c7dd583420ce96d95b14896f005e
ml3907 <35697920+ml3907@users.noreply.github.com>
2018-02-14 17:26:59 -0500

    text1-number and title

--- A3/text1/text1.js
+++ A3/text1/text1.js
@@ -1,42 +1,51 @@
 var headlines = [];
 
-function preload(){
+function preload() {
   var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
   var apikey = "58faa2021ebb4d989a3017d897e08cd9";
   url += "?api-key=" + apikey;
 
-  rytResponse = loadJSON(url);
+  nytResponse = loadJSON(url);
 }
 
-function setup(){
+function setup() {
   createCanvas(640, 1000);
   background(0);
 
   textSize(12);
   textAlign(LEFT);
-  textLeading(20);
-  text(lines, 10, 25);
+  textStyle(ITALIC);
 
   noLoop();
 
   extractHeadlines();
 }
 
-function draw(){
+function draw() {
   background(0);
-  var lineheight = 25
 
-  for (var i = 0; i < headlines.length; i++){
-    //drawheadline
+  var lineheight = 24;
+  var margin = 30;
+  translate(margin, margin);
+
+  //display the titles
+  for (var i = 0; i < headlines.length; i++) {
     fill(255);
-    text(headlines[i], 0, i*lineheight);
+    text(i+1 + ".", 0 , i*lineheight*1.5);
+    if (i < 9) {
+      text(headlines[i], 20 , i*lineheight*1.5);
+    }
+      else{
+        text(headlines[i], 30 , i*lineheight*1.5);
+      }    
   }
 }
 
-function extractHeadlines(){
-  for (var i = 0; i < nytResponse.results.length; i++){
+function extractHeadlines() {
+  // console.log(nytResponse);
+  for (var i = 0; i < nytResponse.results.length; i++) {
     var h = nytResponse.results[i].title;
     append(headlines, h);
   }
-  console.log(headlines);
+  // console.log(headlines);
 }
