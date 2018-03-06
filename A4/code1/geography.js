2651bb45d41fc302b951659d4bb52ba59617a9fa
ml3907 <35697920+ml3907@users.noreply.github.com>
2018-03-06 02:27:37 -0500

    try0306

--- A4/code1/geography.js
+++ A4/code1/geography.js
@@ -1,18 +1,36 @@
 var table;
 var margin = 20;
-var numbers = [];
+var boro = []; type = []; numbers = [];
 
 function preload(){
   table = loadTable('HNYdata.csv', 'csv', 'header');
 }
 
 function setup() {
-  createCanvas(600 , 800);
+  createCanvas(800 , 1000);
+  background(15);
   loadData();
 }
 
 function loadData() {
+  var boro = table.getColumn("Borough");
+  var type = table.getColumn("Type");
   var numbers = table.getColumn("Numbers");
+  
+}
 
-  console.log(numbers);
+function draw() {
+  background(15);
+
+  for (var i = 0; i < boro.length; i++) {
+    if (boro[i] == "Bronx" ) {
+      if (type[i] == "1") {
+        fill(0); //show the new construction with black square 
+      } else if {
+        fill(255); //show the preservation with white square
+      }
+      rect(margin*i, margin*i, numbers[i], numbers[i])
+      }
+    }
+
 }
