704bb89532fb9863e832a8e181190bc91f999c49
ml3907 <35697920+ml3907@users.noreply.github.com>
2018-03-06 00:07:53 -0500

    try0306

--- A4/code1/geography.js
+++ A4/code1/geography.js
@@ -1,44 +1,17 @@
 var table;
-var margin = 30;
-var rectHeight = 10;
+var margin = 20;
+var numbers = [];
 
-var no = [];
-var yes = [];
-
-function preload() {
-  table = loadTable('HYNdata.csv','csv','header')
+function preload(){
+  table = loadTable('HNYdata.csv', 'csv', 'header');
 }
 
 function setup() {
-  createCanvas(window.innerWidth, 800);
-  background(0);  
-  textSize(12);
-  textFont('Helvetica');
-
+  createCanvas(600 , 800);
   loadData();
-  noStroke();
-
 }
 
-function draw() {
-  background(0);
-  var boroughs = table.getColumn("Borough");
-  var com = table.getColumn("Completion");
+function loadData() {
+  var numbers = table.getColumn("Numbers");
 
-  var rental = table.getColumn("Rental");
-  var owner = table.getColumn("Homeownership");
-  
-//for each borough, count the number of completed and uncompleted housing units.
-//for completed housing units, map them by yellow square. the number of square   
-
-  for (var i = 0; i< com.length; i++) {
-    if (com[i] = 1) {
-      append(no, 0);
-    } else {
-      append(yes, 0);
-    }
-  }
-
-  for 
-
-}
+  console.log(numbers);
