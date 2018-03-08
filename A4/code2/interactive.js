326a6b040dfe68974c50f19f63e426c0d7d38afe
ml3907 <35697920+ml3907@users.noreply.github.com>
2018-03-08 01:48:58 -0500

    0308withbug

--- A4/code2/interactive.js
+++ A4/code2/interactive.js
@@ -16,64 +16,52 @@ function setup() {
   createCanvas(window.innerWidth, 1280);
   background(255);
   loadData();
+
+  button = createButton('Check the homeownership');
+  button.position(200, 50);
+  button.mousePressed(change);
+}
+
+function change () { //when mouse is pressed, change to the view of homeownership composition.
+  var rnum = rentals/200;
+  var hnum = homeownership/200;
+  var onum = others/200;
+
+  for (var i = 0; i <= boro.length; i++) {
+    for (var j = 0; j <= rnum[i]; j++) {
+      if (type[i] == "1") {
+      fill(0);//show the rental units.
+      rect(200+10*(j-1), 100+60*i, 5, 5)
+      } else {
+      fill(255, 190, 0);//show the homeownership units.
+      rect(200+10*(j-1), 80+60*i, 5, 5)
+      }
+    }
 }
 
 function draw() {
   background(255);
   noStroke();
   
-  //draw the nested loop
+  //draw the squares
   for (var i = 0; i <= boro.length; i++) {
-    for (var j = 0; j <= squares[i]; j++) { 
-    //for every row(i): let j(max)=squares[i], and for every j<=j(max), draw one square;
-          if (type[i] == "1") {
-            fill(0);//show the new construction units.
-            rect(200+20*(j-1), 100+60*i, 10, 10)
-          } else {
-            fill(255, 190, 0);//show the preservation units.
-            rect(200+20*(j-1), 80+60*i, 10, 10)
-          }
+    for (var j = 0; j <= squares[i]; j++) {
+      if (type[i] == "1") {
+      fill(0);//show the new construction units.
+      rect(200+20*(j-1), 100+60*i, 10, 10)
+      } else {
+      fill(255, 190, 0);//show the preservation units.
+      rect(200+20*(j-1), 80+60*i, 10, 10)
+      }
     }
-   }
+  }
 
-  //draw icons
-  push()
-  fill(0);
-  rect(50, 670, 10, 10);
-  pop();
-  push();
-  fill(255, 190, 0);
-  rect(50, 690, 10, 10);
-  pop();
-
-  //five Boroughs
-  push();
-  fill(0);
-  textSize(20);
-  textFont('Helvetica')
-  text("Bronx", 50, 120);
-  text("Brooklyn", 50, 240);
-  text("Manhattan", 50, 360);
-  text("Queens", 50, 480);
-  text("Staten Island", 50, 600);
-  pop();
-
-  push();
-  fill(0);
-  textSize(16);
-  textFont('Helvetica')
-  textFont('Helvetica')
-  text("*  The Housing New York Units by Building data after January 1, 2014 comes from HPD.", 50, 720)
-  text("New Construction Units", 80, 680);
-  text("Preservation Units", 80, 700);
-  text("*  Click the units to see the homeownership composition. (NOT FINISH YET)", 50, 50)
-  pop();
 }
 
 function loadData() {
   boro = table.getColumn("Borough");
   type = table.getColumn("Type");
-  squares = table.getColumn("Numbers"); 
+  squares = table.getColumn("Numbers");
   rentals = table.getColumn("Rental");
   owners = table.getColumn("Homeownership");
   others = table.getColumn("Others");
