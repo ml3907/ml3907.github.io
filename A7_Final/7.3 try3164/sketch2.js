17373ba34ddb6dc9615d0710445b3662f3cf7143
ml3907 <ml3907@columbia.edu>
2018-04-25 14:09:58 -0400

    0425new button

--- A7_Final/7.3 try3164/sketch2.js
+++ A7_Final/7.3 try3164/sketch2.js
@@ -20,7 +20,7 @@ var minNameLen4, maxNameLen4, minNameLen5, maxNameLen5, minNameLen6, maxNameLen6
 
 var sel, item;
 //define constants
-var state = 0;
+var state = 0, stateHide = 0;
 var docks3164 = 67;
 var docks3255 = 19;
 var docks319 = 33;
@@ -54,6 +54,18 @@ function setup() {
   sel.option("Midtown");
   sel.option("Downtown");
   sel.changed(mySelectEvent);
+
+  button = createButton('Add/Hide');
+  button.position(150, 100);
+  button.mousePressed(nextState);
+}
+
+function nextState() {
+  if (stateHide == 1) {
+    stateHide = 0;
+  } else {
+    stateHide = 1;
+  }
 }
 
 function mySelectEvent() {
@@ -61,24 +73,35 @@ function mySelectEvent() {
   if (item == "Uptown") {
     state = 1;
     clear();
-  } else if (item == "Midtown") {
-    state = 2;
-    clear();
-  } else if (item == "Downtown") {
-    state = 3;
-    clear();
   }
 }
 
+function display11() {
+  for (var i = 0; i <= count1.length; i++) {
+    if (count1[i] = 1) {
+      stroke(3, 169, 244);
+      strokeWeight(count1[i]/5);
+      line(mid+5, 450+2*i, mid+305, 50+lineheight*i);
+      }
+  }
+
+  for (var j = 0; j <= count2.length; j++) {
+    if (count2[j] = 1) {
+      stroke(3, 169, 244);
+      strokeWeight(count2[j]/3);
+      line(mid-300, 100+lineheight*j, mid, 500+2*j);
+    }
+  }
+}
 
 function draw() {
   background(255);
   if (state == 1) {
     drawUptown();
-  } else if (state == 2) {
-    drawMidtown();
-  } else if (state == 3) {
-    drawDowntown();
+  }
+
+  if (stateHide == 1) {
+    display11();
   }
   
   // Settings to map the interactive text
@@ -110,68 +133,6 @@ function draw() {
       minNameLen2 = startname2[i].length;
     } else if (startname2[i].length < minNameLen2) {
       minNameLen2 = startname2[i].length;
-    }
-  }
-
-  // Midtown
-  for (var i = 0; i < count3.length; i++) {
-    rectwidth3 = map(endname3[i].length,minNameLen3, maxNameLen3, 80, 200);
-    if (!maxNameLen3) {
-      maxNameLen3 = endname3[i].length;
-    } else if (endname3[i].length > maxNameLen3) {
-      maxNameLen3 = endname3[i].length;
-    }
-
-    if (!minNameLen3) {
-      minNameLen3 = endname3[i].length;
-    } else if (endname3[i].length < minNameLen3) {
-      minNameLen3 = endname3[i].length;
-    }
-  }
-
-  for (var i = 0; i < count4.length; i++) {
-    rectwidth4 = map(startname4[i].length,minNameLen4, maxNameLen4, 100, 250);
-    if (!maxNameLen4) {
-      maxNameLen4 = startname4[i].length;
-    } else if (startname4[i].length > maxNameLen4) {
-      maxNameLen4 = startname4[i].length;
-    }
-
-    if (!minNameLen4) {
-      minNameLen4 = startname4[i].length;
-    } else if (startname4[i].length < minNameLen4) {
-      minNameLen4 = startname4[i].length;
-    }
-  }
-
-  // Downtown
-  for (var i = 0; i < count5.length; i++) {
-    rectwidth5 = map(endname5[i].length,minNameLen5, maxNameLen5, 80, 200);
-    if (!maxNameLen5) {
-      maxNameLen5 = endname5[i].length;
-    } else if (endname5[i].length > maxNameLen5) {
-      maxNameLen5 = endname5[i].length;
-    }
-
-    if (!minNameLen5) {
-      minNameLen5 = endname5[i].length;
-    } else if (endname5[i].length < minNameLen5) {
-      minNameLen5 = endname5[i].length;
-    }
-  }
-
-  for (var i = 0; i < count6.length; i++) {
-    rectwidth6 = map(startname6[i].length,minNameLen6, maxNameLen6, 100, 250);
-    if (!maxNameLen6) {
-      maxNameLen6 = startname6[i].length;
-    } else if (startname4[i].length > maxNameLen6) {
-      maxNameLen6 = startname6[i].length;
-    }
-
-    if (!minNameLen6) {
-      minNameLen6 = startname6[i].length;
-    } else if (startname6[i].length < minNameLen6) {
-      minNameLen6 = startname6[i].length;
     }
   }
   
@@ -263,164 +224,6 @@ function drawUptown() {
   pop();
 }
 
-//need to change the 系数
-function drawMidtown() {
-  push();
-  //trips that start at id 3255
-  for (var i = 0; i <= count3.length; i++) {
-    if (count3[i] > 1) {
-      stroke(3, 169, 244);
-      strokeWeight(count3[i]/8);
-      line(mid+5, 800+5*i, mid+305, 100+lineheight*i);
-    }
-  }
-  pop();
-
-  push();
-  //display the station name; highlight when mouse move over it
-  for (var i = 0; i < count3.length; i++) {
-    if (count3[i] > 1) {
-      if (mouseX > starttext3 && mouseX < starttext3+180 && mouseY < 100+lineheight*i && mouseY > 100+lineheight*i+(-1*rectheight)) {
-        fill(0);
-        textSize(14);
-        text(endname3[i], starttext3, 100+lineheight*i);
-
-        fill(30, 136, 229);
-        textSize(12);
-        text(count3[i]+ " rides end here", mouseX+rectwidth3, mouseY);
-      } else {
-        fill("grey");
-        textSize(12);
-        text(endname3[i], starttext3, 100+lineheight*i);
-      }
-    }
-  }
-  pop();
-
-    //trips that end at id3255
-  push();
-  for (var j = 0; j <= count4.length; j++) {
-    if (count4[j] > 1) {
-      stroke(3, 169, 244);
-      strokeWeight(count4[j]/8);
-      line(mid-300, 100+lineheight*j, mid, 800+5*j);
-    }
-  }
-  pop();
-
-  push();
-  for (var j = 0; j < count4.length; j++) {
-    if (count4[j] > 1) {
-      if (mouseX > starttext2 && mouseX < starttext2+180 && mouseY > 100+lineheight*j+(-1*rectheight) && mouseY < 100+lineheight*j) {
-        fill(0);
-        textSize(14);
-        text(startname4[j], starttext2, 100+lineheight*j);
-
-        fill(30, 136, 229);
-        textSize(12);
-        text(count4[j]+ " rides start here", mouseX-rectwidth4, 100+lineheight*j);
-      } else {
-        fill("grey");
-        noStroke();
-        textSize(12);
-        text(startname4[j], starttext2, 100+lineheight*j);
-      }
-    }
-  }
-  pop();
-
-  push();
-  fill(30, 136, 229);
-  noStroke();
-  rect(mid, 770, 10, 1320);
-  fill(21, 101, 192);
-  text("8 Ave. W 31 St", stationx+50, stationy);
-  text("19", stationx+80, stationy+20);
-  text("2947", stationx+100, stationy+40);
-  text("155.1", stationx+160, stationy+60);
-  text("1745", stationx+150, stationy+80);
-  text("1202 ", stationx+180, stationy+100);
-  pop();
-}
-
-function drawDowntown() {
-  push();
-  //trips that start at id319
-  for (var i = 0; i <= count5.length; i++) {
-    if (count5[i] > 1) {
-      stroke(3, 169, 244);
-      strokeWeight(count5[i]/5);
-      line(mid+5, 500+2*i, mid+305, 50+lineheight*i);
-      }
-  }
-  pop();
-    
-  push();
-  //display the station name; highlight when mouse move over it
-  for (var i = 0; i < count5.length; i++) {
-    if (count5[i] > 1) {
-      if (mouseX > starttext3 && mouseX < starttext3+180 && mouseY < 50+lineheight*i && mouseY > 50+lineheight*i+(-1*rectheight)) {
-        fill(0);
-        textSize(14);
-        text(endname5[i], starttext3, 50+lineheight*i);
-
-        fill(30, 136, 229);
-        textSize(12);
-        text(count5[i]+ " rides end here", mouseX+rectwidth5, mouseY);
-      } else {
-        fill("grey");
-        textSize(12);
-        text(endname5[i], starttext3, 50+lineheight*i);
-      }
-    }
-  }
-  pop();
-
-//trips that end at id319
-  push();
-  for (var j = 0; j <= count6.length; j++) {
-    if (count6[j] > 1) {
-      stroke(3, 169, 244);
-      strokeWeight(count6[j]/3);
-      line(mid-300, 100+lineheight*j, mid, 500+2*j);
-    }
-  }
-  pop();
-
-  push();
-  for (var j = 0; j < count6.length; j++) {
-    if (count6[j] > 1) {
-      if (mouseX > starttext2 && mouseX < starttext2+180 && mouseY > 100+lineheight*j+(-1*rectheight) && mouseY < 100+lineheight*j) {
-        fill(0);
-        textSize(14);
-        text(startname6[j], starttext2, 100+lineheight*j);
-
-        fill(30, 136, 229);
-        textSize(12);
-        text(count6[j]+ " rides start here", mouseX-rectwidth6, 100+lineheight*j);
-      } else {
-        fill("grey");
-        noStroke();
-        text(startname6[j], starttext2, 100+lineheight*j);
-      }
-    }
-  }
-  pop();
-
-  push();
-  fill(30, 136, 229);
-  noStroke();
-  rect(mid, 490, 10, 230);
-  fill(21, 101, 192);
-  text("Station: Fulton St & Broadway", stationx, stationy);
-  text("33", stationx+80, stationy+20);
-  text("10.79", stationx+100, stationy+40);
-  text("356", stationx+160, stationy+60);
-  text("179", stationx+150, stationy+80);
-  text("177 ", stationx+180, stationy+100);
-  pop();
-}
-
 function loadData() {
   //uptown
   startname1 = table1.getColumn("startstation");
@@ -429,19 +232,5 @@ function loadData() {
   startname2 = table2.getColumn("startstation");
   endname2 = table2.getColumn("endstation");
   count2 = table2.getColumn("count");
-  //console.log(count2);
-  //midtown
-  startname3 = table3.getColumn("startstation");
-  endname3 = table3.getColumn("endstation");
-  count3 = table3.getColumn("count");
-  startname4 = table4.getColumn("startstation");
-  endname4 = table4.getColumn("endstation");
-  count4 = table4.getColumn("count");
-  //downtown
-  startname5 = table5.getColumn("startstation");
-  endname5 = table5.getColumn("endstation");
-  count5 = table5.getColumn("count");
-  startname6 = table6.getColumn("startstation");
-  endname6 = table6.getColumn("endstation");
-  count6 = table6.getColumn("count");
+
 }
