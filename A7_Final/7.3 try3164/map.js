d0a0f7ede0c3745fdc9e1001b702ed62fc249f72
ml3907 <ml3907@columbia.edu>
2018-04-25 17:24:37 -0400

    final update

--- A7_Final/7.3 try3164/map.js
+++
@@ -1,4 +0,0 @@
-function preload() {
-	//ask for api
-	mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' + clon + ',' + clat + ',' + zoom + '/' + ww + 'x' + hh + '?access_token=' + ACCESS_TOKEN);
-}
